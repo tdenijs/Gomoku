5f7c27db677f09b81fb874bdc1a48840c475567c
tdenijs <tristan_de_nijs@yahoo.com>
2017-03-31 10:28:59 -0700

    Added a basic NAIVE (random)AI. Smart AI will be a derived class of the basic Naive AI at a later time.

--- app/src/main/java/tdenijs/gomoku/MainActivity.java
+++ app/src/main/java/tdenijs/gomoku/MainActivity.java
@@ -2,12 +2,51 @@ package tdenijs.gomoku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int players;
+    private int size;
+    private String style;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        players = 1;
+        size = 15;
+        style = "Standard";
         setContentView(R.layout.activity_main);
     }
+
+    public void on1PlayerClick(View v) {
+        players = 1;
+    }
+
+    public void on2PlayerClick(View v) {
+        players = 2;
+    }
+
+    public void on10XClick(View v) {
+        size = 10;
+    }
+
+    public void on15XClick(View v){
+        size = 15;
+    }
+
+    public void on20XClick(View v){
+        size = 20;
+    }
+
+    public void onStandardClick(View v) {
+        style = "Standard";
+    }
+
+    public void onFreestyleClick(View v) {
+        style = "Freestyle";
+    }
+
+
 }
